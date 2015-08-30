5e4ac1bfd3a71279a4359647afb832d7dff5878f
Lucas0231 <lascourslucas@gmail.com>
2015-08-30 23:08:21 +0200

    Update links.rb

--- lib/vlb/shell-commands/links.rb
+++ lib/vlb/shell-commands/links.rb
@@ -2,7 +2,24 @@ module VikiLinkBot
   class Shell
 
     {
-        aide: 'https://github.com/thilp/vlb-rb'
+        aide: 'https://github.com/thilp/vlb-rb',
+        if rep2[3].lower() == ":!rc": 
+                    IRC.send("PRIVMSG %s :%s\r\n" % (rep2[2], "RC : #vikidia-recentchanges .")) 
+ 
+                if rep2[3].lower() == ":!mypage": 
+                    IRC.send("PRIVMSG %s :%s\r\n" % (rep2[2], "https://fr.vikidia.org/wiki/Special:MyPage .")) 
+ 
+                if rep2[3].lower() == ":!gazette": 
+                    IRC.send("PRIVMSG %s :%s\r\n" % (rep2[2], "https://fr.vikidia.org/wiki/Vikidia:Gazette .")) 
+ 
+                if rep2[3].lower() == ":!pàs": 
+                    IRC.send("PRIVMSG %s :%s\r\n" % (rep2[2], "https://fr.vikidia.org/wiki/Vikidia:Pages_%C3%A0_supprimer .")) 
+ 
+                if rep2[3].lower() == ":!block": 
+                    IRC.send("PRIVMSG %s :%s\r\n" % (rep2[2], "Bloquer un utilisateur (administrateurs) : https://fr.vikidia.org/wiki/Sp%C3%A9cial:Bloquer .")) 
+ 
+                if rep2[3].lower() == ":!accueil": 
+                    IRC.send("PRIVMSG %s :%s\r\n" % (rep2[2], "https://fr.vikidia.org/wiki/Vikidia:Accueil .")) 
     }.each do |k, v|
       class_eval <<-RUBY
         def #{k}(m, *_)
