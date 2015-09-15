3493586f4431bd27558b3b2ee0e6fb52b5183f71
thilp <thilp@thilp.net>
2015-09-15 16:42:45 +0200

    split shell-core.rb: move Input in its own file

--- lib/vlb/shell-core/input.rb
+++ lib/vlb/shell-core/input.rb
@@ -1,16 +1,7 @@
-require 'cinch'
-require 'httpclient'
-require 'vlb/utils'
 require 'vlb/wiki'
-
-unless $DONT_LOAD_SHELL_COMMANDS
-  Dir.glob(__dir__ + '/shell-commands/*.rb').each do |fname|
-    require fname
-  end
-end
+require 'vlb/utils'
 
 module VikiLinkBot
-
   class Input
     include Enumerable
 
@@ -65,43 +56,5 @@ module VikiLinkBot
     def empty?
       @tokens.empty?
     end
-
   end
-
-  class Shell
-    include Cinch::Plugin
-
-    match //, strip_colors: true, method: :read_eval
-
-    def initialize(*_)
-      super
-      @httpc = HTTPClient.new
-    end
-
-    def read_eval(m, super_tokens=nil)
-      input = super_tokens || Input.new(m.message)
-
-      return if input.empty?
-
-      if public_methods(false).include?(input.command)
-        debug "Running command #{input.command} with arguments #{input.args} and wikis #{input.wikis.map(&:base_url)}"
-        begin
-          method(input.command).call(m, input)
-        rescue => e
-          m.reply "Oups ! Exception non rattrapée : #{e}"
-          log(e.message + ' -- ' + e.backtrace.join("\n"))
-        end
-      else
-        guesses = Utils.guess(input.command.to_s, public_methods(false)).map { |g| "!#{g}" }
-        m.reply "Vouliez-vous dire #{Utils.join_multiple(guesses)} ?" unless guesses.empty?
-      end
-
-    end
-
-    def version(m, tokens)
-      m.reply 'VikiLinkBot::Shell 2.4.1 — « Songe à la douceur »'
-    end
-
-  end
-
 end
---
+++ lib/vlb/shell-core/shell-core.rb
@@ -0,0 +1,50 @@
+require 'cinch'
+require 'httpclient'
+require 'vlb/shell-core/input'
+require 'vlb/utils'
+
+unless $DONT_LOAD_SHELL_COMMANDS
+  Dir.glob(__dir__ + '/shell-commands/*.rb').each do |fname|
+    require fname
+  end
+end
+
+module VikiLinkBot
+
+  class Shell
+    include Cinch::Plugin
+
+    match //, strip_colors: true, method: :read_eval
+
+    def initialize(*_)
+      super
+      @httpc = HTTPClient.new
+    end
+
+    def read_eval(m, super_tokens=nil)
+      input = super_tokens || Input.new(m.message)
+
+      return if input.empty?
+
+      if public_methods(false).include?(input.command)
+        debug "Running command #{input.command} with arguments #{input.args} and wikis #{input.wikis.map(&:base_url)}"
+        begin
+          method(input.command).call(m, input)
+        rescue => e
+          m.reply "Oups ! Exception non rattrapée : #{e}"
+          log(e.message + ' -- ' + e.backtrace.join("\n"))
+        end
+      else
+        guesses = Utils.guess(input.command.to_s, public_methods(false)).map { |g| "!#{g}" }
+        m.reply "Vouliez-vous dire #{Utils.join_multiple(guesses)} ?" unless guesses.empty?
+      end
+
+    end
+
+    def version(m, tokens)
+      m.reply 'VikiLinkBot::Shell 2.4.1 — « Songe à la douceur »'
+    end
+
+  end
+
+end
