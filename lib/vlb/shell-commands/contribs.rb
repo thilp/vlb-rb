9b02b80298807045573422afcb7abbbc82a823ca
thilp <thilp@thilp.net>
2015-10-04 02:04:07 +0200

    update !contribs according to the new Command spec

--- lib/vlb/shell-commands/contribs.rb
+++ lib/vlb/shell-commands/contribs.rb
@@ -1,27 +1,27 @@
+require 'vlb/shell-core/command'
+require 'vlb/shell-core/command-dispatchers/cartesian'
+require 'vlb/shell-core/command-filters/nonempty-args'
+
 module VikiLinkBot
   class Shell
 
-    def contribs(m, input)
-      if input.args.empty?
-        m.reply 'Donnez-moi au moins un pseudonyme ou une IP !'
-        return
-      end
-      input.args.uniq.each do |username|
-        input.wikis.uniq.each do |wiki|
-          if username =~ / \d{1,3} (?: \.\d{1,3} ){3} /x # IPv4
-            editcount = '?'
-          else
-            json = wiki.api(action: 'query', list: 'users', ususers: username, usprop: 'editcount')
-            editcount = begin
-              json['query']['users'][0]['editcount'] || raise
-            rescue
-              m.reply "#{username} n'existe pas sur #{wiki.domain}"
-              next
-            end
-          end
-          m.reply "#{editcount} - #{wiki.article_url('Special:Contributions/' + username)}"
+    COMMANDS[:contribs] = Command.new(Dispatcher::CartesianProduct, Filter::NonemptyArgs) do |m, wiki, username|
+      if username =~ / \d{1,3} (?: \.\d{1,3} ){3} /x # IPv4
+        editcount = '?'
+      else
+        json = wiki.api(action: 'query', list: 'users', ususers: username, usprop: 'editcount')
+        editcount = begin
+          json['query']['users'][0]['editcount'] || raise
+        rescue
+          m.reply "#{username} n'existe pas sur #{wiki.domain}"
+          next
         end
       end
+      m.reply "#{editcount} - #{wiki.article_url('Special:Contributions/' + username)}"
+    end
+
+    COMMANDS[:contribs].define_singleton_method(:empty_args_action) do |m, _|
+      m.reply 'Donnez-moi au moins un pseudonyme ou une IP !'
     end
 
   end
