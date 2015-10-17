9027ab0e478536774cae5c6bc7537482d824fd17
thilp <thilp@thilp.net>
2015-10-17 02:39:56 +0200

    StatusChecker bugfix: don't try to ack unless there is an actual @last_statuses

--- lib/vlb/status-checker.rb
+++ lib/vlb/status-checker.rb
@@ -107,7 +107,7 @@ module VikiLinkBot
     # @param [String] host
     # @return [TrueClass,FalseClass] whether the acknowledgement was valid and thus recorded
     def self.ack_last_error(host)
-      return false unless @last_statuses.keys.include?(host)
+      return false unless @last_statuses && @last_statuses.keys.include?(host)
       @acknowledged_errors[host] = @last_statuses[host]
       true
     end
